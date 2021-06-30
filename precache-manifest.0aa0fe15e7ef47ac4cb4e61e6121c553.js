c5b3b497ef7b9d7149b1c114f36887dc3a35b42e
ac88 <mmaxpokerr@gmail.com>
2021-06-30 10:00:22 +0300

    Updates

---
+++ precache-manifest.0aa0fe15e7ef47ac4cb4e61e6121c553.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "297b3cfba90892c87da8",
+    "url": "/food-project/static/css/main.e55088e1.chunk.css"
+  },
+  {
+    "revision": "297b3cfba90892c87da8",
+    "url": "/food-project/static/js/main.f59e5693.chunk.js"
+  },
+  {
+    "revision": "c3999605755585583bb4",
+    "url": "/food-project/static/js/runtime~main.5fda05b1.js"
+  },
+  {
+    "revision": "1e136fbf2d369c6eb810",
+    "url": "/food-project/static/js/2.65aca708.chunk.js"
+  },
+  {
+    "revision": "f7b3fc7a8998e65ebab24eba57167a72",
+    "url": "/food-project/index.html"
+  }
+];
